2e72fcb039c496aa4a2880a9994c3d26673b3552
Mohammad <bagwanmahammad02@gmail.com>
2020-02-05 20:25:29 +0530

    added link in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
+          <Link to="/">Click Here</Link>
           <Route exact path="/" component={Visits} />
         </div>
       </header>
